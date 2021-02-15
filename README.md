653aa96f984ce1c02b734da27c855c2c5d107e60
ACC1100 <aaron.chao2000@gmail.com>
2021-02-15 22:52:47 +1100

    add new config parameters, save/resume search, update readme

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Python script to automatically connect with other second degree connections on L
 WARNING: RUNNING THIS SCRIPT MAY GET YOU BANNED ON LINKEDIN, [see original repo for more details](https://github.com/tomquirk/linkedin-api).
 
 # API
-This script forks [Tom Quirk's original-unofficial-linkedin-api](https://github.com/tomquirk/linkedin-api) with some slight modifications. Since adding connections is not supported in the master branch, and [abinpaul1](https://github.com/tomquirk/linkedin-api/pull/136)'s pull request has not been merged yet, I have manually applied the merge. I have also fixed a bug in another section of the code. 
+This script forks [Tom Quirk's original-unofficial-linkedin-api](https://github.com/tomquirk/linkedin-api) with some slight modifications. Since adding connections is not supported in the master branch, and [abinpaul1](https://github.com/tomquirk/linkedin-api/pull/136)'s pull request has not been merged yet, I have manually applied the merge. I have also fixed a bug in another section of the code. Additionally, the constant `_MAX_REPEATED_REQUESTS` in `linkedin.py` was change from `200` to `50` to speed up search process
 
 Nothing else was changed, so as such, any issues which persist with the master branch of [linkedin-api](https://github.com/tomquirk/linkedin-api) will still persist in this version.
 
@@ -35,14 +35,23 @@ OR alternatively, install API via github: `pip install 'git+https://github.com/A
 - Include a connection message when sending connections
 - Change the lower and upper bound on time between connection requests
 
-## Explanation of default values:
+## Explanation of default values and config parameters:
 ### SEARCH
-- `keywords = ` change keywords to search for, leave empty for no keywords
-##### THE FOLLOWING PARAMETERS ARE INPUTTED AS LISTS, PLEASE ENSURE ALL VALUES ARE SEPARATED BY COMMAS IF CHANGING
+- `keywords = ` Change keywords to search for
+    - Leave empty for no keywords
+- `keyword_title = ` Change keywords to search for in TITLE
+    - Leave empty for no keywords
+##### FOR THE FOLLOWING PARAMETERS, PLEASE ENSURE ALL VALUES ARE SEPARATED BY COMMAS
 - `network_depths = S` S for second, use O for third+
-- `regions = 101452733` 101452733, for australia, to find more region codes, do a search on linkedin website and look at the URL
-- `schools = 10231, 10242` 10231, 10242, for Melbourne/Monash uni, to find more school codes, do a search on linkedin website and look at the URL
+- `regions = 101452733` 101452733, for Australia, 
+    - To find more region codes, do a search on the LinkedIn website with a country filter and find the corresponding value in the URL
+- `schools = ` E.g. 10231, for Monash University, Australia. 
+    - To find more school codes, do a search on LinkedIn website with a school filter and find the corresponding value in the URL
 ### CONNECTIONS
-- `random = False` randomise order which connections are sent based on search results, false: no randomise, add connections sequentially from first to last in order provided in search results,  true: add connections in random order from search results
-- `message =` change the connection message, default empty
+- `random = False` randomise order which connections are sent based on search results
+    - False: no randomise, add connections sequentially from first to last in order provided in search results
+    - True: add connections in random order from search results
+    - CASE SENSITIVE
+- `message =` change the connection message, limited to 140 characters.
+    - Default empty
 - `lower_wait_time = 5` `upper_wait_time = 50` change the time between connection requests, time is randomised between lower and upper bounds
--- autoconnect.py
+++ autoconnect.py
@@ -2,9 +2,9 @@ import json
 import configparser
 import random
 import time
+import signal
+import sys
 from linkedin_api import Linkedin
-
-# print(config['SEARCH']['keywords'])
 
 class Auto_Connector:
     def __init__(self) -> None:
@@ -25,14 +25,16 @@ class Auto_Connector:
         print('successfully authenticated')
             
     def _create_config(self):
+        """Creates search config file"""
         print('no config detected')
 
         self.config.add_section('SEARCH')
         self.config.set('SEARCH', '# see: https://github.com/ACC1100/linkedin-auto_connect#Config for explanation of values and how to customise')
         self.config.set('SEARCH', 'keywords', '')
+        self.config.set('SEARCH', 'keyword_title', '')
         self.config.set('SEARCH', 'network_depths', 'S')
         self.config.set('SEARCH', 'regions', '101452733')
-        self.config.set('SEARCH', 'schools', '10231, 10242')
+        self.config.set('SEARCH', 'schools', '')
 
         self.config.add_section('CONNECTIONS')
         self.config.set('CONNECTIONS', 'random', 'False')
@@ -48,6 +50,7 @@ class Auto_Connector:
         print('successfully created new config')
     
     def _create_credentials(self):
+        """Creates login credentials file in PLAIN TEXT"""
         print('no credentials detected')
         username = input("enter username/email: ")
         password = input("enter password: ")
@@ -63,29 +66,63 @@ class Auto_Connector:
         return new_credentials
 
     def search(self):
+        """Searches for people based on config parameters, writes results to JSON and returns results"""
         print("running search")
         results = self.linkedin.search_people(
                     keywords=self.config["SEARCH"]['keywords'],
                     network_depths=self._string_to_list(self.config["SEARCH"]['network_depths']),
                     regions=self._string_to_list(self.config["SEARCH"]['regions']),
                     schools=self._string_to_list(self.config["SEARCH"]['schools']),
-                    profile_languages=["en"])
+                    profile_languages=["en"],
+                    keyword_title=self.config["SEARCH"]['keyword_title'])
         print("finished search")
+
+        with open("search_results.json", "w") as f:
+            json.dump(results, f, ensure_ascii=True, indent=4)
+        print("wrote search results to file")
+
         return results
     
     def _string_to_list(self, string):
-        """converts items separated by comma in string to list form"""
-        return list(map(lambda x: x.strip(), string.split(',')))
+        """converts items separated by comma in string to list form, returns None if string is empty"""
+        output = list(map(lambda x: x.strip(), string.split(',')))
+        if len(output) == 0:
+            return None
+        else:
+            return output
 
-    def connect(self, search_results):
+    def connect(self):
+        try:
+            with open("search_results.json", "r") as f:
+                search_results = json.load(f)
+            
+            print("existing search results found, continue with existing results?")
+            print("or create new results?")
+            print("0: continue, 1: create new")
+            response = input()
+
+            if response == "0":
+                search_results = search_results
+            elif response == "1":
+                search_results = self.search()
+            else:
+                print("invalid response, closing")
+                return
+
+        except FileNotFoundError:
+            search_results = self.search()
+
         print("initiating auto-connect")
         print("ctrl+c to cancel at anytime")
+
+        fail_count = 0
+        self.search_results = search_results
         while True:
             if self.config["CONNECTIONS"]['random'] == "True":
-                item = random.choice(search_results)
+                item = random.choice(self.search_results)
             else:
-                item = search_results[0]
-            search_results.remove(item) # yep, this is slow, search results are in list form
+                item = self.search_results[0]
+            self.search_results.remove(item) # probably slow
 
             urn = item['urn_id']
             pid = item['public_id']
@@ -93,14 +130,35 @@ class Auto_Connector:
                 status = self.linkedin.add_connection(pid, profile_urn=urn, message=self.config["CONNECTIONS"]['message'])
                 if status is True:
                     print(f"Error connecting with: {pid}")
+                    fail_count += 1
                 else:
                     print(f"Success: {pid}")
+                    fail_count = 0
             
                 wait = round(random.random() * random.randrange(int(self.config["CONNECTIONS"]['lower_wait_time']), int(self.config["CONNECTIONS"]['upper_wait_time'])), 2)
                 print(f"waiting {wait} seconds")
                 time.sleep(wait)
+            
+            if fail_count >= 5:
+                print("ERROR: FAILED 5 CONSECUTIVE CONNECTIONS,")
+                print("you are likely to be rate limited,")
+                print("double check by manually sending a connection through LinkedIn website")
+                print("EXITING")
+                return
+
+
+        
+
+    def signal_handler(self, sig, frame):
+        with open("search_results.json", "w") as f:
+            f.seek(0)
+            json.dump(self.search_results, f, ensure_ascii=True, indent=4)
+        print('successfully saved updated search before exiting')
+        sys.exit(0)
+
 
 
 if __name__ == "__main__":
     connector = Auto_Connector()
-    connector.connect(connector.search())
+    signal.signal(signal.SIGINT, connector.signal_handler)
+    connector.connect()
